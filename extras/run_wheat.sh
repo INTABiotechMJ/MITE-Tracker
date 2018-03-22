460b855472c8cf727b5006611d01af32acef5a72
juan <juan@Juans-MacBook-Pro.local>
2018-03-22 09:06:08 -0300

    initial

--- extras/run_wheat.sh
+++ extras/run_wheat.sh
@@ -1,26 +1,26 @@
-python FAspliter.py -s ../data/Triticum_aestivum.TGACv1.dna.toplevel.fa  -o  ../data/TGAC_by_chr/ -l 1A 1B 1D 2A 2B 2D 3A 3B 3D 4A 4B 4D 5A 5B 5D 6A 6B 6D 7A 7B 7D
-python ../MITETracker.py -g ../data/TGAC_by_chr/1A.fasta -w 2 -j TGAC_1A --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/1B.fasta -w 2 -j TGAC_1B --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/1D.fasta -w 2 -j TGAC_1D --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/2A.fasta -w 2 -j TGAC_2A --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/2B.fasta -w 2 -j TGAC_2B --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/2D.fasta -w 2 -j TGAC_2D --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/3A.fasta -w 2 -j TGAC_3A --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/3B.fasta -w 2 -j TGAC_3B --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/3D.fasta -w 2 -j TGAC_3D --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/4A.fasta -w 2 -j TGAC_4A --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/4B.fasta -w 2 -j TGAC_4B --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/4D.fasta -w 2 -j TGAC_4D --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/5A.fasta -w 2 -j TGAC_5A --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/5B.fasta -w 2 -j TGAC_5B --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/5D.fasta -w 2 -j TGAC_5D --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/6A.fasta -w 2 -j TGAC_6A --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/6B.fasta -w 2 -j TGAC_6B --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/6D.fasta -w 2 -j TGAC_6D --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/7A.fasta -w 2 -j TGAC_7A --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/7B.fasta -w 2 -j TGAC_7B --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/7D.fasta -w 2 -j TGAC_7D --task candidates
-python ../MITETracker.py -g ../data/TGAC_by_chr/U.fasta -w 2 -j TGAC_U --task candidates
+python FAspliter.py -s ../../data/Triticum_aestivum.TGACv1.dna.toplevel.fa -o ../../data/TGAC_by_chr/ -l 1A 1B 1D 2A 2B 2D 3A 3B 3D 4A 4B 4D 5A 5B 5D 6A 6B 6D 7A 7B 7D
+python ../MITETracker.py -g ../../data/TGAC_by_chr/1A.fasta -w 2 -j TGAC_1A --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/1B.fasta -w 2 -j TGAC_1B --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/1D.fasta -w 2 -j TGAC_1D --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/2A.fasta -w 2 -j TGAC_2A --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/2B.fasta -w 2 -j TGAC_2B --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/2D.fasta -w 2 -j TGAC_2D --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/3A.fasta -w 2 -j TGAC_3A --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/3B.fasta -w 2 -j TGAC_3B --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/3D.fasta -w 2 -j TGAC_3D --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/4A.fasta -w 2 -j TGAC_4A --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/4B.fasta -w 2 -j TGAC_4B --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/4D.fasta -w 2 -j TGAC_4D --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/5A.fasta -w 2 -j TGAC_5A --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/5B.fasta -w 2 -j TGAC_5B --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/5D.fasta -w 2 -j TGAC_5D --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/6A.fasta -w 2 -j TGAC_6A --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/6B.fasta -w 2 -j TGAC_6B --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/6D.fasta -w 2 -j TGAC_6D --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/7A.fasta -w 2 -j TGAC_7A --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/7B.fasta -w 2 -j TGAC_7B --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/7D.fasta -w 2 -j TGAC_7D --task candidates
+python ../MITETracker.py -g ../../data/TGAC_by_chr/U.fasta -w 2 -j TGAC_U --task candidates
 mkdir ../TGAC
 cat ../results/TGAC_*/candidates.csv > ../results/TGAC/candidates.csv
 python ../MITETracker.py -g none -w 4 -j TGAC --task cluster --min_copy_number 4
