b9c5ba0f5a9ac173f54b7fa030d42fc54a2cb92c
juan <juan@Juans-MacBook-Pro.local>
2018-05-21 13:38:49 -0300

    initial

--- extras/compare.sh
+++
@@ -1,5 +0,0 @@
-python compare_db.py  -e2 data/mitehunter/all.fa  -e1 data/tracker/families_nr.fasta -e1n MITE_Tracker -e2n MITE_Hunter -l B -o news
-
-python compare_db.py  -e2 data/repbase/repbase_nonautonomous.fasta   -e1 data/detectmite/rice.mite.fasta  -e1n detectMITE -e2n Repbase -l B -o news
-
-python compare_db.py  -e2 data/repbase/repbase_nonautonomous.fasta   -e1 data/mitehunter/all.fa  -e1n MITE_Hunter -e2n Repbase -l A -o newsdsa
